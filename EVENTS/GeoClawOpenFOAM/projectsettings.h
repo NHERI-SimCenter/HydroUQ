528ef6eec9e9c747ab2ad7eb8b29de9358b91415
fmckenna <fmckenna@berkeley.edu>
2020-10-13 13:35:44 -0700

    fmk - changes to HydroUQ to make like EE and WE

---
+++ EVENTS/GeoClawOpenFOAM/projectsettings.h
@@ -0,0 +1,33 @@
+#ifndef PROJECTSETTINGS_H
+#define PROJECTSETTINGS_H
+
+#include <QFrame>
+#include <QFileDialog>
+#include <QDir>
+#include <iostream>
+#include <fstream>
+
+namespace Ui {
+class projectsettings;
+}
+
+class projectsettings : public QFrame
+{
+    Q_OBJECT
+
+public:
+    explicit projectsettings(int, QWidget *parent = nullptr);
+    ~projectsettings();
+    bool getData(QMap<QString, QString>&,int);
+    void refreshData(int);
+
+private slots:
+    void on_Btn_WDir_clicked();
+
+private:
+    void hideshowelems(int);
+    Ui::projectsettings *ui;
+    QUrl workdirUrl; // Defaulty workdirectory
+};
+
+#endif // PROJECTSETTINGS_H
---
+++ EVENTS/GeoClawOpenFOAM/qt3dframe.cpp
@@ -0,0 +1,94 @@
+#include <QWidget>
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QCheckBox>
+#include <QCommandLinkButton>
+
+#include <Qt3DRender/qcamera.h>
+#include <Qt3DCore/qentity.h>
+#include <Qt3DRender/qcameralens.h>
+#include <Qt3DInput/QInputAspect>
+
+#include <Qt3DExtras/qtorusmesh.h>
+#include <Qt3DRender/qmesh.h>
+#include <Qt3DRender/qtechnique.h>
+#include <Qt3DRender/qmaterial.h>
+#include <Qt3DRender/qeffect.h>
+#include <Qt3DRender/qtexture.h>
+#include <Qt3DRender/qrenderpass.h>
+#include <Qt3DRender/qsceneloader.h>
+#include <Qt3DRender/qpointlight.h>
+
+#include <Qt3DCore/qtransform.h>
+#include <Qt3DCore/qaspectengine.h>
+
+#include <Qt3DRender/qrenderaspect.h>
+#include <Qt3DExtras/qforwardrenderer.h>
+
+#include <Qt3DExtras/qt3dwindow.h>
+#include <Qt3DExtras/qfirstpersoncameracontroller.h>
+
+#include <QtGui/QScreen>
+
+#include "qt3dframe.h"
+#include "scenemodifier.h"
+
+QT3DFrame::QT3DFrame(QWidget *parent)
+    : QWidget(parent)
+{
+
+    this->initUI();
+
+}
+
+void QT3DFrame::initUI()
+{
+    Qt3DExtras::Qt3DWindow *view = new Qt3DExtras::Qt3DWindow();
+    view->defaultFrameGraph()->setClearColor(QColor(QRgb(0x4d4d4f)));
+    QWidget *container = QWidget::createWindowContainer(view);
+    QSize screenSize = view->screen()->size();
+    container->setMinimumSize(QSize(200, 100));
+    container->setMaximumSize(screenSize);
+
+    QHBoxLayout *hLayout = new QHBoxLayout(this);
+    QVBoxLayout *vLayout = new QVBoxLayout();
+    vLayout->setAlignment(Qt::AlignTop);
+    hLayout->setContentsMargins(0,0,0,0);
+    hLayout->addWidget(container, 1);
+    hLayout->addLayout(vLayout);
+
+    //this->setWindowTitle(QStringLiteral("Basic shapes"));
+
+    Qt3DInput::QInputAspect *input = new Qt3DInput::QInputAspect;
+    view->registerAspect(input);
+
+    // Root entity
+    Qt3DCore::QEntity *rootEntity = new Qt3DCore::QEntity();
+
+    // Camera
+    Qt3DRender::QCamera *cameraEntity = view->camera();
+
+    cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
+    cameraEntity->setPosition(QVector3D(0, 0, 20.0f));
+    cameraEntity->setUpVector(QVector3D(0, 1, 0));
+    cameraEntity->setViewCenter(QVector3D(0, 0, 0));
+
+    Qt3DCore::QEntity *lightEntity = new Qt3DCore::QEntity(rootEntity);
+    Qt3DRender::QPointLight *light = new Qt3DRender::QPointLight(lightEntity);
+    light->setColor("white");
+    light->setIntensity(1);
+    lightEntity->addComponent(light);
+    Qt3DCore::QTransform *lightTransform = new Qt3DCore::QTransform(lightEntity);
+    lightTransform->setTranslation(cameraEntity->position());
+    lightEntity->addComponent(lightTransform);
+
+    // For camera controls
+    Qt3DExtras::QFirstPersonCameraController *camController = new Qt3DExtras::QFirstPersonCameraController(rootEntity);
+    camController->setCamera(cameraEntity);
+
+    // Scenemodifier
+    SceneModifier *modifier = new SceneModifier(rootEntity);
+
+    // Set root object of the scene
+    view->setRootEntity(rootEntity);
+}
