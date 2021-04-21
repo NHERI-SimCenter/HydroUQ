f2aeacf5a33cd110898fe939e3c4dc364564f7ed
Ajay B Harish <ajayharish.accts@gmail.com>
2021-04-20 21:03:26 -0700

    Update to wrapper.sh

--- Agave/wrapper.sh
+++ Agave/wrapper.sh
@@ -4,71 +4,77 @@
 set -x
 ${AGAVE_JOB_CALLBACK_RUNNING}
 
-#pause() {
-#	read -n1 -rsp $'Press any key to continue or Ctrl+C to exit...\n'
-#}
-
-# Temporarily defining variables here
-#inputDirectory="/work/07231/ajaybh/stampede2/trywrapper01"
-#EVENTAPP="GeoClawOpenFOAM"
-
 # Change to input directory
 cd "${inputDirectory}"
 pwd
 ls -al
-echo ${inputDirectory}
 
 # unzip template dir
 unzip templatedir.zip
 rm templatedir.zip
 cd ..
-echo "Templatedir unzipped"
+echo "Template directory unzipped"
 
 #Unzip utils
 cd utils
 unzip '*.zip'
 rm *.zip
 cd ..
-echo "utils unzipped"
+echo "Utilities unzipped"
 
 #Add utils to PATH
 export PATH=$PATH:$PWD/utils
 chmod +x utils/*
 
-# Set the BIM file path to use
+# Set the paths for: (a) Input file (b) Input directors
+# (c) Any external libraries
 export BIM=${inputDirectory}/templatedir/dakota.json
 export HYDROPATH=${inputDirectory}/templatedir
-echo $BIM
-echo $HYDROPATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/OpenFOAMExtensions
+echo "New variables to folders have been created"
 
-echo "Extracting EVENT app using jq"
-#Extracting the first event app
+# Set paths for the HydroUQ at the simcenter backend
+HydroBrain=/home1/00477/tg457427/SimCenterBackendApplications/v2.3.0/applications/createEVENT/GeoClawOpenFOAM
+echo "Path to HydroBrain initialize"
+
+# Load the OpenFoam module
+module load intel/18.0.2
+module load impi/18.0.2
+module load openfoam/7.0
+module load python3
+echo "Modules have been loaded on Stampede2"
+
+# Install necessary 
+
+echo "Necessary python modules installed for the user"
+
+# Export the paths to load OpenFOAM
+export FOAM_USER_LIBBIN=/work/projects/wma_apps/stampede2/openfoam7/lib
+export FOAM_USER_APPBIN=/work/projects/wma_apps/stampede2/openfoam7/bin
+export PATH=$PATH:${FOAM_USER_APPBIN}
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${FOAM_USER_LIBBIN}
+echo "OpenFOAM paths loaded"
+
+# Get information about EVENT
+echo "Starting to extracting EVENT app using jq"
+
+# Extracting the first event app
 EVENTAPP=$(jq -r .Events[0].Application $BIM)
 echo "Event application detected is $EVENTAPP"
-echo $EVENTAPP
 
-if [[ $EVENTAPP == "GeoClawOpenFOAM" ]]; then
-	echo "Event is processing"
+# GeoClawOpenFOAM event
+if [[ $EVENTAPP == "GeoClawOpenFOAM" ]]
+then
+	
+	# Start event processing
+	echo "GeoClawOpenFOAM event processing has started"
 
 	# Run the processing file
 	# This will generate all the OpenFOAM dictionaries
 	# by reading the dakota.json file
 	# python Processor.py ${inputDirectory}/templatedir/dakota.json
-	python Processor.py $BIN 
-
-	# Load the OpenFoam module
-	module load intel/18.0.2
-	module load impi/18.0.2
-	module load openfoam/7.0
-	echo "Modules have been loaded on Stampede2"
-
-	# Export the paths to load olaFoam
-	export FOAM_USER_LIBBIN=/work/projects/wma_apps/stampede2/openfoam7/lib
-	export FOAM_USER_APPBIN=/work/projects/wma_apps/stampede2/openfoam7/bin
-	export PATH=$PATH:${FOAM_USER_APPBIN}
-	export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${FOAM_USER_LIBBIN}
-	echo "olafoam paths loaded"
-	echo "Testing successfully complete"
+	# python Processor.py $BIM
+	python3 $HydroBrain/Processor.py $BIM
 
 	# Meshing with OpenFOAM (or conversion)
 	MESHTYPE=$(jq -r .Events[0].MeshType $BIM)
@@ -88,18 +94,20 @@ if [[ $EVENTAPP == "GeoClawOpenFOAM" ]]; then
 		MESHSOFT=$(jq -r .Events[0].MeshSoftware $BIM)
 		# Get the file name
 		MESHSOFTFILE=$(jq -r .Events[0].MeshFile $BIM)
+		# Get the path of the file exactle
+		MESHFILE=${inputDirectory}/templatedir/$MESHSOFTFILE
 		# Create the mesh files using the mesh
 		# that have been imported from other softwares
 		if [[ $MESHSOFT == "0" ]]; then
-			fluentMeshToFoam $MESHSOFTFILE > fluentMeshToFoam.log
+			fluentMeshToFoam $MESHFILE > fluentMeshToFoam.log
 		elif [[ $MESHSOFT == "1" ]]; then
-			ideasToFoam $MESHSOFTFILE > ideasToFoam.log
+			ideasToFoam $MESHFILE > ideasToFoam.log
 		elif [[ $MESHSOFT == "2" ]]; then
-			cfx4ToFoam $MESHSOFTFILE > cfx4ToFoam.log
+			cfx4ToFoam $MESHFILE > cfx4ToFoam.log
 		elif [[ $MESHSOFT == "3" ]]; then
-			gambitToFoam $MESHSOFTFILE > gambitToFoam.log
+			gambitToFoam $MESHFILE > gambitToFoam.log
 		elif [[ $MESHSOFT == "4" ]]; then
-			gmshToFoam $MESHSOFTFILE > gmshToFoam.log
+			gmshToFoam $MESHFILE > gmshToFoam.log
 		fi
 		# Log in the event
 		echo "Mesh import complete"
@@ -111,7 +119,7 @@ if [[ $EVENTAPP == "GeoClawOpenFOAM" ]]; then
 		snappymeshfile=snappyHexMeshDict
 		if [[ -f "$blockmeshfile" ]]; then
 			# Copy the files to system folder
-			mv blockMeshDict system/blockMeshDict
+			mv templatedir/blockMeshDict system/blockMeshDict
 			# Run the command
 			blockMesh > blockMesh.log
 			# Log in the event
@@ -119,7 +127,7 @@ if [[ $EVENTAPP == "GeoClawOpenFOAM" ]]; then
 			# Check if snappy needed
 			if [[ -f "$snappymeshfile" ]]; then
 				# Copy the files to system folder
-				mv snappyHexMeshDict system/snappyHexMeshDict
+				mv templatedir/snappyHexMeshDict system/snappyHexMeshDict
 				# Run the command
 				snappyHexMesh > snappyHexMesh.log
 				# Log in the event
@@ -131,6 +139,20 @@ if [[ $EVENTAPP == "GeoClawOpenFOAM" ]]; then
 		fi
 	fi
 
+	# Create the 0-folder
+	rm -fr 0
+	cp -r 0.org 0
+
+	# Setting up the fields
+	setFields > setFields.log
+	echo "Fields have been set successfully"
+
+	# Decomposte for parallel processing
+	decomposePar > decomposePar.log
+	echo "Domain has been decomposed"
+
+	# Starting CFD simulations
+	ibrun olaDyMFlow -parallel > olaDyMFlow.log
 
 elif [[ $EVENTAPP == "Preprocess" ]]; then
 	echo "Event is pre-processing"
