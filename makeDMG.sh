8bc97aa122b85ebd9dadabc35622a166f14419b4
fmckenna <fmckenna@berkeley.edu>
2024-04-11 08:49:32 -0700

    fmk - updating makeDMG.sh

--- makeDMG.sh
+++ makeDMG.sh
@@ -56,84 +56,6 @@ fi
 
 macdeployqt ./Hydro_UQ.app 
 
-#
-# copy files from VTK dir
-#
-
-cp ../../VTK/lib/libvtkgl2ps-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingGL2PSOpenGL2-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkChartsCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonColor-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonComputationalGeometry-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonDataModel-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonExecutionModel-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonMath-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonMisc-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonSystem-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkCommonTransforms-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkDICOMParser-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersExtraction-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersGeneral-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersGeometry-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersHybrid-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersHyperTree-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersImaging-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersModeling-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersSources-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersStatistics-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkFiltersTexture-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkGUISupportQt-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkIOCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkIOGeometry-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkIOImage-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkIOLegacy-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkIOXML-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkIOXMLParser-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkImagingColor-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkImagingCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkImagingGeneral-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkImagingHybrid-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkImagingSources-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkInfovisCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkInfovisLayout-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkInteractionStyle-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkInteractionWidgets-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkParallelCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkParallelDIY-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingAnnotation-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingContext2D-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingContextOpenGL2-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingContextOpenGL2-9.2.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingFreeType-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingHyperTreeGrid-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingLabel-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingOpenGL2-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingUI-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingVolume-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkRenderingVolume-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkViewsCore-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkViewsInfovis-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkViewsQt-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkdoubleconversion-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkexpat-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkfmt-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkfreetype-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkglew-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkjpeg-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkkissfft-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkloguru-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtklz4-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtklzma-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkmetaio-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkpng-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkpugixml-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtksys-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtktiff-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-cp ../../VTK/lib/libvtkzlib-9.2.1.dylib ./Hydro_UQ.app/Contents/Frameworks
-
 # copy applications folderm opensees and dakota
 echo "cp -fR $pathToBackendApps/applications ./$APP_FILE/Contents/MacOS"
 cp -fR $pathToBackendApps/applications ./$APP_FILE/Contents/MacOS
@@ -223,15 +145,12 @@ if [ "${DMG_METHOD}" == "NEW" ]; then
 	--window-size 600 350 \
 	--no-internet-enable \
 	--icon-size 125 \
-	--icon "../icons/NHERI-WEuq-Icon.ico" 125 130 \
-	--hide-extension "${APP_NAME}.app" \
+	--icon "${APP_FILE}" 125 130 \
+	--hide-extension "${APP_FILE}" \
 	--app-drop-link 450 130 \
 	--codesign $appleCredential \
 	"${DMG_FILENAME}" \
 	"app"
-
-    #  --icon "${APP_NAME}.app" 125 130 \	
-    #  --notarize $appleID $appleAppPassword \
 	
     mv ./app/$APP_FILE ./
     rm -fr app
